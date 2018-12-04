5da410a793d751b2b9334d1ecba6e1d0a35e8f36
Ernesto Guardado <ernesto.guardado@Ernesto-Macbook-Pro-2.local>
2018-12-03 16:39:46 -0800

    Working push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
-
-
-import { AppComponent } from './app.component';
-
-import {AngularFireModule} from '@angular/fire'
-import {AngularFirestoreModule} from '@angular/fire/firestore'
-
-import {environment} from '../environments/environment';
-import { BoardsComponent } from './boards/boards.component';
-import { BoardsDetailComponent } from './boards-detail/boards-detail.component';
-import { BoardsCreateComponent } from './boards-create/boards-create.component';
-import { BoardsEditComponent } from './boards-edit/boards-edit.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
   MatTableModule,
   MatIconModule,
   MatButtonModule,
   MatCardModule,
-} from '@angular/material'
-import { ReactiveFormsModule } from '@angular/forms';
+  MatFormFieldModule } from "@angular/material";
+
+import { AppComponent } from './app.component';
+import { BoardsComponent } from './boards/boards.component';
+import { BoardsDetailComponent } from './boards-detail/boards-detail.component';
+import { BoardsCreateComponent } from './boards-create/boards-create.component';
+import { BoardsEditComponent } from './boards-edit/boards-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
   {
     path: 'boards',
     component: BoardsComponent,
-    data: { title: 'Boards List' },
+    data: { title: 'Boards List' }
   },
   {
     path: 'boards-details/:id',
     component: BoardsDetailComponent,
-    data: { title: 'Boards Details' },
+    data: { title: 'Boards Details' }
   },
   {
     path: 'boards-create',
     component: BoardsCreateComponent,
-    data: { title: 'Create Boards' },
+    data: { title: 'Create Boards' }
   },
   {
     path: 'boards-edit/:id',
     component: BoardsEditComponent,
-    data: { title: 'Edit Boards' },
+    data: { title: 'Edit Boards' }
   },
-  { path: '', redirectTo: '/boards', pathMatch: 'full' },
-]
+  { path: '',
+    redirectTo: '/boards',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -57,16 +56,21 @@ const appRoutes: Routes = [
     BoardsEditComponent
   ],
   imports: [
-    BrowserModule,
     RouterModule.forRoot(appRoutes),
-    AngularFireModule.initializeApp(environment.firebase), 
-    AngularFirestoreModule, BrowserAnimationsModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     MatInputModule,
     MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    ReactiveFormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/boards-edit/boards-edit.component.ts
+++ src/app/boards-edit/boards-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FirestoreService } from '../firestore.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-boards-edit',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardsEditComponent implements OnInit {
 
-  constructor() { }
+  boardsForm: FormGroup;
+  id:string = '';
+  title:string = '';
+  description:string = '';
+  author:string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute, private fs: FirestoreService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.getBoard(this.route.snapshot.params['id']);
+    this.boardsForm = this.formBuilder.group({
+      'title' : [null, Validators.required],
+      'description' : [null, Validators.required],
+      'author' : [null, Validators.required]
+    });
+  }
+
+  getBoard(id) {
+    this.fs.getBoard(id).subscribe(data => {
+      this.id = data.key;
+      this.boardsForm.setValue({
+        title: data.title,
+        description: data.description,
+        author: data.author
+      });
+    });
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.fs.updateBoards(this.id, form)
+      .subscribe(res => {
+          this.router.navigate(['/boards']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  boardsDetails() {
+    this.router.navigate(['/boards-details', this.id]);
   }
 
 }
